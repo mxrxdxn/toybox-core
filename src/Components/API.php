40a3f3477af37d192c2d30bed1e23970fd0a3a60
Ron Marsden <rm@maxwebsolutions.com>
2023-07-03 15:19:46 +0100

    Add new API component
    Signed-off-by: Ron Marsden <rm@maxwebsolutions.com>

---
+++ src/Components/API.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Toybox\Core\Components;
+
+use Closure;
+use Toybox\Core\Exceptions\InvalidMethodException;
+
+class API
+{
+    /**
+     * Adds an endpoint to the WP-JSON API.
+     *
+     * When you define your callback, make sure you pass in the argument for $request, which should be of type
+     * WP_REST_Request.
+     *
+     * @see https://developer.wordpress.org/rest-api/extending-the-rest-api/adding-custom-endpoints/
+     *
+     * @param string  $endpoint
+     * @param Closure $callback
+     * @param string  $namespace
+     * @param string  $method
+     *
+     * @return void
+     * @throws InvalidMethodException
+     */
+    public static function addEndpoint(string $endpoint, Closure $callback, string $namespace = "toybox/v1", string $method = "GET"): void
+    {
+        $allowedMethods = ["GET", "POST", "PUT", "HEAD", "DELETE", "PATCH", "OPTIONS"];
+        $method         = strtoupper($method);
+
+        if (! in_array($method, $allowedMethods)) {
+            $validMethods = implode("\", \"", $allowedMethods);
+            throw new InvalidMethodException("The method \"{$method}\" is not valid. Valid options are \"{$validMethods}\".");
+        }
+
+        add_action("rest_api_init", function () use ($namespace, $endpoint, $callback, $method) {
+            register_rest_route($namespace, $endpoint, [
+                "methods"  => $method,
+                "callback" => $callback,
+            ]);
+        });
+    }
+}
---
+++ src/Exceptions/InvalidMethodException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Toybox\Core\Exceptions;
+
+class InvalidMethodException extends \Exception {}
