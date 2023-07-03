97c08deccf623d5034462a31ea583b0481abe044
Ron Marsden <rm@maxwebsolutions.com>
2023-07-03 14:15:44 +0100

    Add new preloading method
    Signed-off-by: Ron Marsden <rm@maxwebsolutions.com>

--- src/Components/HTTP.php
+++ src/Components/HTTP.php
@@ -19,4 +19,38 @@ class HTTP
             header("Link: <{$asset}>; {$hints}", false);
         });
     }
+
+    /**
+     * Adds preloading for scripts and styles using native functionality.
+     *
+     * @return void
+     */
+    public static function preload(): void
+    {
+        add_action('wp_head', function () {
+            global $wp_styles, $wp_scripts;
+
+            $html = "";
+
+            // Styles
+            if (is_iterable($wp_styles->queue)) {
+                foreach ($wp_styles->queue as $handle) {
+                    if (! empty($wp_styles->registered[$handle]->src)) {
+                        $html .= "<link rel='preload' as='style' href='{$wp_styles->registered[$handle]->src}'>" . PHP_EOL;
+                    }
+                }
+            }
+
+            // Scripts
+            if (is_iterable($wp_scripts->queue)) {
+                foreach ($wp_scripts->queue as $handle) {
+                    if (! empty($wp_scripts->registered[$handle]->src)) {
+                        $html .= "<link rel='preload' as='script' href='{$wp_scripts->registered[$handle]->src}'>" . PHP_EOL;
+                    }
+                }
+            }
+
+            echo $html;
+        }, 1);
+    }
 }
