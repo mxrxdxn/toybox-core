77e6d060affbc8e2abd8d78f61af6758163c4641
Ron Marsden <rm@maxwebsolutions.com>
2023-10-16 13:37:26 +0100

    Add permission_callback to API component registration
    Signed-off-by: Ron Marsden <rm@maxwebsolutions.com>

--- src/Components/API.php
+++ src/Components/API.php
@@ -17,13 +17,14 @@ class API
      *
      * @param string  $endpoint
      * @param Closure $callback
+     * @param Closure $permissionCallback
      * @param string  $namespace
      * @param string  $method
      *
      * @return void
      * @throws InvalidMethodException
      */
-    public static function addEndpoint(string $endpoint, Closure $callback, string $namespace = "toybox/v1", string $method = "GET"): void
+    public static function addEndpoint(string $endpoint, Closure $callback, Closure $permissionCallback, string $namespace = "toybox/v1", string $method = "GET"): void
     {
         $allowedMethods = ["GET", "POST", "PUT", "HEAD", "DELETE", "PATCH", "OPTIONS"];
         $method         = strtoupper($method);
@@ -33,10 +34,11 @@ class API
             throw new InvalidMethodException("The method \"{$method}\" is not valid. Valid options are \"{$validMethods}\".");
         }
 
-        add_action("rest_api_init", function () use ($namespace, $endpoint, $callback, $method) {
+        add_action("rest_api_init", function () use ($namespace, $endpoint, $permissionCallback, $callback, $method) {
             register_rest_route($namespace, $endpoint, [
                 "methods"  => $method,
                 "callback" => $callback,
+                "permission_callback" => $permissionCallback(),
             ]);
         });
     }
