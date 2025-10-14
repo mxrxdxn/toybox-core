7feec30e1678b399dad857440edb27c2fc15c955
Ron Marsden <rm@maxwebsolutions.com>
2025-10-14 10:33:15 +0100

    Add method to hide default WordPress dashboard widgets
    Introduce `hideWidgets` method to remove default widgets like Quick Draft, WordPress Events, News, and Activity from the admin dashboard, simplifying the interface for users.

--- src/Components/Dashboard.php
+++ src/Components/Dashboard.php
@@ -23,4 +23,21 @@ class Dashboard
             });
         });
     }
+
+    /**
+     * Hides specific widgets from the WordPress dashboard.
+     *
+     * This method removes certain default widgets such as Quick Draft, WordPress Events and News,
+     * and Activity from being displayed on the WordPress admin dashboard to streamline the interface for users.
+     *
+     * @return void
+     */
+    public static function hideWidgets()
+    {
+        add_action("wp_dashboard_setup", function () {
+            remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
+            remove_meta_box('dashboard_primary', 'dashboard', 'side');
+            remove_meta_box('dashboard_activity', 'dashboard', 'normal');
+        });
+    }
 }
